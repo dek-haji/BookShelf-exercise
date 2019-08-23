815026b88124ad0f0e265597e79e4c31c73c0952
dek-haji <deeeqhaji@gmail.com>
2019-08-23 12:10:47 -0500

    Add project files.

---
+++ BookShelf/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Google.GData.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Models
+{
+    public class ApplicationUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(FullName = "FullName")]
+        public string FullName => $"{FirstName} {LastName}";
+
+    }
+}
---
+++ BookShelf/Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Models
+{
+    public class Author
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string PrefferedGenre { get; set; }
+    }
+}
